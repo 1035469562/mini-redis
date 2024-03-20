8e12fb61327bf184bff72e46421db22f63db0c07
Jcarry <1035469562@qq.com>
2024-03-20 10:04:38 +0800

    init test

---
+++ mini-redis/target/debug/deps/mini_redis.d
@@ -0,0 +1,5 @@
+C:\Users\Administrator\Desktop\mini-redis\mini-redis\target\debug\deps\mini_redis.exe: src\main.rs
+
+C:\Users\Administrator\Desktop\mini-redis\mini-redis\target\debug\deps\mini_redis.d: src\main.rs
+
+src\main.rs:
---
+++ mini-redis/target/debug/mini-redis.d
@@ -0,0 +1,1 @@
+C:\Users\Administrator\Desktop\mini-redis\mini-redis\target\debug\mini-redis.exe: C:\Users\Administrator\Desktop\mini-redis\mini-redis\src\main.rs
